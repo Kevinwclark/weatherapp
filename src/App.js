db3a46362f83c55ff1326c5ad711aa11fe993219
Kevin Clark <kevinwilliamclark@gmail.com>
2022-04-08 21:48:59 -0500

    🐣 gets weather but only location for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Button } from '@mui/material';
+
 
 function App() {
+
+  const [weather , setWeather] = useState(null);
+  console.log(weather);
+
+  const getWeather = () => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=46.563328&lon=-94.2112768&appid=${API_KEY}`, requestOptions)
+      .then(response => response.text())
+      .then(result => setWeather(JSON.parse(result)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+    <h1>Weather</h1>
+    <Button variant="contained" color="primary" onClick={getWeather}>
+      Get Weather
+    </Button>
+    <p>{weather && weather.name}</p>
+     
     </div>
   );
 }
