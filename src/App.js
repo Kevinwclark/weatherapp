9f65506548860ec5eb508cb9a840c83396b0246a
Kevin Clark <kevinwilliamclark@gmail.com>
2022-04-09 11:18:05 -0500

    Sets temps 🌡 and converts to from Kelvin to F.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import './App.css';
 import { useState } from 'react';
 import { Button } from '@mui/material';
+const Converter = require("node-temperature-converter");
 
 
 function App() {
-
-  const [weather , setWeather] = useState(null);
-  console.log(weather);
+  const [temps, setTemps] = useState({
+    feels_like: 0,
+    humidity: 0,
+    pressure: 0,
+    temp: 0,
+    temp_max: 0,
+    temp_min: 275.51
+  });
 
   const getWeather = () => {
     var requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
+  const API_KEY = ''
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=46.563328&lon=-94.2112768&appid=${API_KEY}`, requestOptions)
       .then(response => response.text())
-      .then(result => setWeather(JSON.parse(result)))
-  }
+      .then(result => JSON.parse(result))
+      .then(result => {
+        console.log(result);
+        setTemps({
+          feels_like: new Converter.Kelvin(result.main.feels_like).toFahrenheit(),
+          humidity: result.main.humidity,
+          pressure: result.main.pressure,
+          temp: new Converter.Kelvin(result.main.temp).toFahrenheit(),
+          temp_max: new Converter.Kelvin(result.main.temp_max).toFahrenheit(),
+          temp_min: new Converter.Kelvin(result.main.temp_min).toFahrenheit()
+        });
+      },
+      console.log(temps)
+      )}
 
   return (
     <div className="App-header">
@@ -25,8 +44,12 @@ function App() {
     <Button variant="contained" color="primary" onClick={getWeather}>
       Get Weather
     </Button>
-    <p>{weather && weather.name}</p>
-     
+    <p>Feels Like: {temps.feels_like}</p>
+    <p>Humidity: {temps.humidity}</p>
+    <p>Pressure: {temps.pressure}</p>
+    <p>Temperature: {temps.temp}</p>
+    <p>Max Temperature: {temps.temp_max}</p>
+    <p>Min Temperature: {temps.temp_min}</p>
     </div>
   );
 }
